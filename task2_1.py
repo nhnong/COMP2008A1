743120058fff9b17ccfe8553bc32fec622b8fe5f
Nhi Nong <nong242005@gmail.com>
2025-04-12 16:54:55 +1000

    cleamed task2_1 12/04/25

--- task2_1.py
+++ task2_1.py
@@ -21,9 +21,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from nltk.stem import WordNetLemmatizer
 
 # ----------------------------------------------------------------------------
-
-# ----------------------------------------------------------------------------
-# root function for task2_1
+# root function for task2_1, calls other functions to process the data and create a word cloud
 def task2_1():
     accidents_data = pd.read_csv('accident.csv')
     accidents_data = casefolding_and_noise(accidents_data)
@@ -34,42 +32,29 @@ def task2_1():
     return word_cloud
 
 # ----------------------------------------------------------------------------
-# casefolding for all columns, converting all string values to lowercase
-# noise removal for all columns, removing all non-alphanumeric characters
+# casefolding and noise removal for all columns
 def casefolding_and_noise(accidents_data):
     for column in accidents_data.columns:
         accidents_data[column] = accidents_data[column].apply(lambda x: x.lower() if type(x)==str else x)
         accidents_data[column] = accidents_data[column].apply(lambda x: re.sub(r'[^A-Za-z\s]', '', x) if type(x)==str else x)
     return accidents_data
-#for column in accidents_data.columns:
-#    accidents_data[column] = accidents_data[column].apply(lambda x: x.lower() if type(x)==str else x)
-#    accidents_data[column] = accidents_data[column].apply(lambda x: re.sub(r'[^A-Za-z\s]', '', x) if type(x)==str else x)
 
 # ----------------------------------------------------------------------------
-
-# turning the column accidents_data['DCA_DESC'] into tokens, and remove stop words
+# turning the column accidents_data['DCA_DESC'] into tokens, remove stop words and apply stemming
 porter_stemmer = PorterStemmer()
 def stopwords_and_stem(text):
     tokens = nltk.word_tokenize(text)
     tokens = [word for word in tokens if word not in stop_words]
     stemmed = ' '.join([porter_stemmer.stem(word) for word in tokens])
-    # put non stop words into list and join them back into a string
-    #text = ' '.join([word for word in stemmed if word not in stop_words])
     return stemmed
-
 
 # ----------------------------------------------------------------------------
 # bag of words, coverts text into BoW, where the matrix counts the number of times each word appears in the text
-
 vectorizer = CountVectorizer()
 def bag_of_words(dca_des):
     bow = vectorizer.fit_transform(dca_des)
     pd.DataFrame(bow.toarray())
     return bow
-
-#bow = vectorizer.fit_transform(accidents_data['DCA_DESC'])
-#vocabulary = vectorizer.get_feature_names_out()
-#pd.DataFrame(bow.toarray())
 
 # ----------------------------------------------------------------------------
 # getting most common words in the vectorizer
@@ -80,13 +65,8 @@ def word_counts_dataframe(bow):
     word_counts = word_counts.sort_values(by='count', ascending=False)
     return word_counts
 
-
-#word_counts = np.asarray(bow.sum(axis=0)).flatten()
-#word_counts = pd.DataFrame(word_counts, index=vocabulary, columns=['count'])
-#word_counts = word_counts.sort_values(by='count', ascending=False)
-
+# ----------------------------------------------------------------------------
 # word cloud, visualisation of the most common words in the text
-
 def creating_word_cloud(word_counts):
     word_cloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(word_counts['count'].to_dict())
     plt.figure(figsize=(10, 5))
@@ -98,15 +78,6 @@ def creating_word_cloud(word_counts):
     plt.savefig(output_filename, dpi=300, bbox_inches='tight')
     return word_cloud
 
-
-#word_cloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(word_counts['count'])
-#plt.figure(figsize=(10, 5))
-#plt.title('Word Cloud of Most Common Words in DCA_DESC')
-#plt.imshow(word_cloud, interpolation='bilinear')
-#plt.axis('off')
-
-#output_filename = 'task2_1_word_cloud.png'
-#plt.savefig(output_filename, dpi=300, bbox_inches='tight')
 
 
 
