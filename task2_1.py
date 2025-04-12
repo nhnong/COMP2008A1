4ca522418824f461e00b781e9e2914f48f1d8863
Nhi Nong <nong242005@gmail.com>
2025-04-12 16:10:48 +1000

    combined some functions together

--- task2_1.py
+++ task2_1.py
@@ -30,92 +30,53 @@ print('initial')
 # ----------------------------------------------------------------------------
 
 # casefolding for all columns, converting all string values to lowercase
-for column in accidents_data.columns:
-    accidents_data[column] = accidents_data[column].apply(lambda x: x.lower() if type(x)==str else x)
-print('casefolding done')
-
-# ----------------------------------------------------------------------------
-
 # noise removal for all columns, removing all non-alphanumeric characters
 for column in accidents_data.columns:
+    accidents_data[column] = accidents_data[column].apply(lambda x: x.lower() if type(x)==str else x)
     accidents_data[column] = accidents_data[column].apply(lambda x: re.sub(r'[^A-Za-z\s]', '', x) if type(x)==str else x)
-print('noise removal done')
+
 
 # ----------------------------------------------------------------------------
 
 # turning the column accidents_data['DCA_DESC'] into tokens, and remove stop words
-def remove_stopwords(text):
-    tokens = nltk.word_tokenize(text)
-    # put non stop words into list and join them back into a string
-    text = ' '.join([word for word in tokens if word not in stop_words])
-    return text
-
-accidents_data['DCA_DESC'] = accidents_data['DCA_DESC'].apply(lambda x: remove_stopwords(x) if type(x)==str else x)
-print('stop words removed')
-
-# ----------------------------------------------------------------------------
-print(accidents_data['DCA_DESC'][0:10])
-# applied stemming to get root form of words
-# ended up with some words that did not make sense within context - e.g. "accid", "anoth"
 porterStemmer = PorterStemmer()
-
-def stem_text(text):
+def stopwords_and_stem(text):
     tokens = nltk.word_tokenize(text)
-    stemmed = [porterStemmer.stem(word) for word in tokens]
-    text = ' '.join(stemmed)
-    return text
+    tokens = [word for word in tokens if word not in stop_words]
+    stemmed = ' '.join([porterStemmer.stem(word) for word in tokens])
+    # put non stop words into list and join them back into a string
+    #text = ' '.join([word for word in stemmed if word not in stop_words])
+    return stemmed
 
-
-accidents_data['DCA_DESC'] = accidents_data['DCA_DESC'].apply(lambda x: stem_text(x) if type(x)==str else x)
-
-print('stemming done')
-
-# ----------------------------------------------------------------------------
-# LEMMATIZATION (doesn't work)
-
-#def lemmatize_text(text):
-#    lemmatizer = WordNetLemmatizer()
-#    tokens = nltk.word_tokenize(text)
-#    lemmatized = [lemmatizer.lemmatize(word) for word in tokens]
-#    text = ' '.join(lemmatized)
-#    return text
-
-#accidents_data['DCA_DESC'] = accidents_data['DCA_DESC'].apply(lambda x: lemmatize_text(x) if type(x)==str else x)
-
-#print('lemmatizing done')
+accidents_data['DCA_DESC'] = accidents_data['DCA_DESC'].apply(lambda x: stopwords_and_stem(x) if type(x)==str else x)
 
 # ----------------------------------------------------------------------------
-
 # bag of words, coverts text into BoW, where the matrix counts the number of times each word appears in the text
-print(len(accidents_data['DCA_DESC']))
 
 vectorizer = CountVectorizer()
 bow = vectorizer.fit_transform(accidents_data['DCA_DESC'])
 vocabulary = vectorizer.get_feature_names_out()
 pd.DataFrame(bow.toarray())
 
-print('bag of words done')
-
 # ----------------------------------------------------------------------------
 # getting most common words in the vectorizer
 word_counts = np.asarray(bow.sum(axis=0)).flatten()
 
-# vocab = rows, column = freq
+# vocab = rows, column = freq, sort by highest frequency
 word_counts = pd.DataFrame(word_counts, index=vocabulary, columns=['count'])
 word_counts = word_counts.sort_values(by='count', ascending=False)
-print(word_counts[:19])
 
-# NEXT: word cloud, visualisation of the most common words in the text
-# wordcloud = WordCloud(width=800, height=400, background_color='white').generate(' '.join(accidents_data['DCA_DESC']))
-
+# word cloud, visualisation of the most common words in the text
 word_cloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(word_counts['count'])
 plt.figure(figsize=(10, 5))
 plt.title('Word Cloud of Most Common Words in DCA_DESC')
 plt.imshow(word_cloud, interpolation='bilinear')
 plt.axis('off')
-plt.show()
-plt.savefig('wordcloud_dca_des.png', dpi=300, bbox_inches='tight')
+
+output_filename = 'task2_1_word_cloud.png'
+plt.savefig(output_filename, dpi=300, bbox_inches='tight')
+
 
 def task2_1():
-    
-    return
+
+    return 
